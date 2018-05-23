12348aa8eb11970a0b9175f1969388f696a82bc0
Sungmin_Encore <lmsgsm1@gmail.com>
2018-05-23 17:21:44 +0900

    로거 설정 파일 수정

--- NewSpring/src/main/java/com/spring/example/HomeController.java
+++ NewSpring/src/main/java/com/spring/example/HomeController.java
@@ -39,7 +39,9 @@ public class HomeController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
-		
+		logger.warn("this is warn");
+		logger.error("this is error");
+		logger.debug("this is debug");
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		

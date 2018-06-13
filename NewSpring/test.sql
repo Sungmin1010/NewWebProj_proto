4b67091ac53b626205606a02b71aa0b1deb8b446
Sungmin-PC <lmsgsm1@gmail.com>
2018-06-14 01:26:35 +0900

    AOP 테스트용 테이블 추가

--- NewSpring/test.sql
+++ NewSpring/test.sql
@@ -95,3 +95,30 @@ SELECT LAST_INSERT_ID();
 update tb_board set hit=hit+1 where bseq=1;
 select * from tb_board
 update tb_board set content="³¯¾¾µµ ´ş°í ¿¡¾îÄÁ »§»§ÇÑµ¥¼­ ¸¸È­Ã¥ º¸°í ½ÍÀºµ¥ ½ÅÃÌ¿¡ ±¦ÂúÀ» °÷ ÀÖÀ»±î¿ä?? " where bseq=20;
+==========================================================
+create table tbl_user(
+uid varchar(50) primary key not null,
+upw varchar(50) not null,
+uname varchar(100) not null,
+upoint int default 0
+);
+create table tbl_message(
+mid int not null auto_increment,
+targetid varchar(50) not null,
+sender varchar(50) not null,
+message text not null,
+opendate DATETIME,
+senddate DATETIME not null default CURRENT_TIMESTAMP,
+primary key(mid)
+);
+alter table tbl_message add constraint fk_usertarget foreign key (targetid) references tbl_user(uid);
+alter table tbl_message add constraint fk_usersender foreign key (sender) references tbl_user(uid);
+
+insert into tbl_user(uid, upw, uname) values('user00', 'user00', 'IRON MAN');
+insert into tbl_user(uid, upw, uname) values('user02', 'user02', 'CAPTAIN');
+insert into tbl_user(uid, upw, uname) values('user03', 'user03', 'HULK');
+insert into tbl_user(uid, upw, uname) values('user04', 'user04', 'THOR');
+insert into tbl_user(uid, upw, uname) values('user10', 'user10', 'QUICK SILVER');
+
+select * from tbl_user;
+select * from tbl_message;

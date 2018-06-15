06bfbd30e1780cd879eba0b5c563524677498f36
Sungmin-PC <lmsgsm1@gmail.com>
2018-06-15 17:02:12 +0900

    aop와 트랜잭션

--- NewSpring/src/main/java/com/spring/example/aopsample/MessageServiceImpl.java
+++ NewSpring/src/main/java/com/spring/example/aopsample/MessageServiceImpl.java
@@ -2,6 +2,7 @@ package com.spring.example.aopsample;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class MessageServiceImpl implements MessageService {
@@ -11,6 +12,7 @@ public class MessageServiceImpl implements MessageService {
 	@Autowired
 	private PointDAO pointDAO;
 	
+	@Transactional
 	@Override
 	public void addMessage(MessageVO vo) throws Exception {
 		messageDAO.create(vo);
